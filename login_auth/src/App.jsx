9816ab5c80f2b644172560df4185dace221fddc3
Sivaranjani <sivaranjanidec2005@gmail.com>
2026-06-10 10:11:39 +0530

    update

--- login_auth/src/App.jsx
+++ login_auth/src/App.jsx
@@ -1,7 +1,3 @@
-import { Analytics } from "@vercel/analytics/next"
-
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
 import { useState } from "react";
 
 import Login from "./pages/Login";
